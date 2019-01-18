Fix bug, improve sea ice deformation processign and temporal scaling of SAR.

sid_deform.py
@@ -12,6 +12,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 #select lenght scale
 lscale = [10,25,50,100,200,500][1]
+print(lscale)
 
 #-------------------------------------------------------------------
 inpath = '../output/drift_'+str(lscale)+'/'
@@ -20,7 +21,7 @@ outpath = outpath_def+'plots/'
 metfile = '../data/10minute_nounits.csv'
 reg = 'leg1'
 
-outname_td = 'td_'+reg+'_L'+str(lscale)+'_25km.csv'
+outname_td = 'td_'+reg+'_L'+str(lscale)+'_15km.csv'
 td_list=[]
 ls_list=[]
 ang_list=[]
@@ -30,19 +31,25 @@ date_list=[]
 fl = sorted(glob(inpath+'*_upm.npy'))
 for i in fl:
     #read in all the data
+    print(i)
     u = np.load(i)
     v = np.load(i.split('_upm')[0]+'_vpm.npy')          #in m/s???
     rpm = np.load(i.split('_upm')[0]+'_rpm.npy')       #cross-correlation matrix
-    lat = np.load(i.split('_upm')[0]+'_lat1pm.npy')      #consider using starting coordinates instead
+    lat = np.load(i.split('_upm')[0]+'_lat1pm.npy')
     lon = np.load(i.split('_upm')[0]+'_lon1pm.npy')
+    
+    lat2 = np.load(i.split('_upm')[0]+'_lat2pm.npy')
+    lon2 = np.load(i.split('_upm')[0]+'_lon2pm.npy')
     
     #get time difference
     date1 = i.split('/')[-1].split('_')[1]
     date2 = i.split('/')[-1].split('_')[2]
     dt1 = datetime.strptime(date1, "%Y%m%dT%H%M%S")
     dt2 = datetime.strptime(date2, "%Y%m%dT%H%M%S")
+    
+    if dt1 > datetime(2015,2,12): continue
 
-    diff = (dt2-dt1).seconds
+    diff = (dt2-dt1).seconds + (dt2-dt1).days*24*60*60
     
     #Lance postion (from Lance's met system)
     mettime = getColumn(metfile,0)
@@ -67,13 +74,19 @@ for i in fl:
     #reproject vertices
     x, y = m(lon, lat)
     
+    #recalculate u,v
+    x2, y2 = m(lon2, lat2)
+    u = (x2-x)/diff
+    v = (y2-y)/diff
+    
     #reproject Lance position
     xl, yl = m(Lance_lon, Lance_lat)
 
-    
+
     #cut out region
-    radius = 25000
+    radius = 15000
     mask = (x<xl-radius) | (x>xl+radius) | (y<yl-radius) | (y>yl+radius)
+    
     x = np.ma.array(x,mask=mask)
     x = np.ma.compressed(x)
     y = np.ma.array(y,mask=mask)
@@ -102,7 +115,10 @@ for i in fl:
     
     #mask out all poor quality data: rpm > 0.4
     mask = rpm > 0.3
-    u = np.ma.array(u,mask=~mask)
+    try:
+        u = np.ma.array(u,mask=~mask)       #sometimes problems with the mask - as if gridding of lat2pm is not always perfect!
+    except:
+        continue
     u = np.ma.compressed(u)
     v = np.ma.array(v,mask=~mask)
     v = np.ma.compressed(v)

sid_drift.py
@@ -36,7 +36,7 @@ from sea_ice_drift import SeaIceDrift
 #el: only image from early morning and late afteroon (same date)
 #24h: only early mornming images (2 different dates)
 #all drift files will be in same folder files with same time steps will be overwritten - finally there will be no double files!
-mode = ['all', 'ee', 'el', '24h'][3]
+mode = ['all', 'ee', 'el', '24h', '2d', '3d'][2]
 print(mode)
 
 #and number of grid points in each direction
@@ -44,7 +44,7 @@ print(mode)
 #lsc_list = [25,50,100,200,500]   #not ls but number of nominal grid points
 #minlen = [4,2,1,.5,.2]
 #maxlen = [6,3,1.5,.75,.3]
-resolution = 200
+resolution = 25
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 outpath_drift = '../output/drift_'+str(resolution)+'/'
 outpath = '../plots/drift_'+str(resolution)+'/'
@@ -91,7 +91,7 @@ for i in range(0,len(fl)):
     dt1 = dl[i]
     if mode != 'all':
         #check if the file is early morning
-        if dt1.hour > 8: continue
+        if dt1.hour > 7: print('not early morning');continue
 
         #select dt2, depending on the mode
         dt_diff = np.array(dl) - dt1
@@ -104,9 +104,15 @@ for i in range(0,len(fl)):
         #24h: expected time difference: 22-24 h 
         elif mode =='24h':
             match = np.argmin(abs(dt_diff - timedelta(hours=22)))
-        
-        print(match)
-    
+            if dl[match].hour > 7: print('second file not early morning');continue
+        #2days
+        elif mode =='2d':
+            match = np.argmin(abs(dt_diff - timedelta(hours=46)))
+            if dl[match].hour > 7: print('second file not early morning');continue
+        #3days
+        elif mode =='3d':
+            match = np.argmin(abs(dt_diff - timedelta(hours=70)))
+            if dl[match].hour > 7: print('second file not early morning');continue
     else:
         match = i+1
     

sid_power_law_time_sar.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 fig1 = plt.figure(figsize=(9,9))
 fig1 = plt.figure(figsize=(7.5,7))
 ax = fig1.add_subplot(111)
-title = 'ship_radar+buoys+SAR'
+#title = 'ship_radar+buoys+SAR_faithful'
+title = 'ship_radar+buoys+SAR_bold2'
 #title = 'sar'
 ax.set_title(title,fontsize=29, loc='left')
 ax.set_xlabel(r"Time scale (h)",fontsize=25)
@@ -44,68 +45,77 @@ for i in range(0,len(lsc_list)):
     #ax.scatter(ls, td, lw=0, alpha=.2)  # Data
     ax.plot(meanls,meantd,'s',markersize=7,markeredgecolor='k')
 
-##buoy data - do we have enough of 1 day data (should be enough for the entire leg 1)
-##scales 2-100km
-#inpath = '../data/buoys/'
-#fname_start = 'nice1_in_SAR'
-#lsc_list = [1,3,6,12,24]
+#buoy data - do we have enough of 1 day data (should be enough for the entire leg 1)
+#scales 2-100km
+inpath = '../data/buoys/'
+fname_start = 'nice1_in_SAR'
+lsc_list = [1,3,6,12,24]
 
-#for i in lsc_list:
-    #scale = i
-    #print(scale)
-    #fname_td1 = inpath+'dr_'+fname_start+'1_'+str(scale)+'h'
-    #fname_ls1 = inpath+'ls_'+fname_start+'1_'+str(scale)+'h'
-    #fname_td2 = inpath+'dr_'+fname_start+'2_'+str(scale)+'h'
-    #fname_ls2 = inpath+'ls_'+fname_start+'2_'+str(scale)+'h'
+for i in lsc_list:
+    scale = i
+    print(scale)
     
-    #td = np.append(np.load(fname_td1),np.load(fname_td2))/24/60/60      #convert from days to s
-    #ls = np.append(np.load(fname_ls1),np.load(fname_ls2))
+    fname_td1 = inpath+'dr_'+fname_start+'1_'+str(scale)+'h'
+    fname_ls1 = inpath+'ls_'+fname_start+'1_'+str(scale)+'h'
+    fname_td2 = inpath+'dr_'+fname_start+'2_'+str(scale)+'h'
+    fname_ls2 = inpath+'ls_'+fname_start+'2_'+str(scale)+'h'
     
-    #print(len(ls))
+    td = np.append(np.load(fname_td1,encoding='latin1'),np.load(fname_td2,encoding='latin1'))/24/60/60      #convert from days to s
+    ls = np.append(np.load(fname_ls1,encoding='latin1'),np.load(fname_ls2,encoding='latin1'))
     
-    ##throw away very high deformation rates (unrealistic values)
-    #mask = td>10e-3
-    #ls = np.ma.array(ls,mask=mask)
-    #td = np.ma.array(td,mask=mask)
-    #ls = np.ma.compressed(ls)
-    #td = np.ma.compressed(td)   
+    print(len(ls))
+    
+    #throw away very high deformation rates (unrealistic values)
+    mask = td>10e-3
+    ls = np.ma.array(ls,mask=mask)
+    td = np.ma.array(td,mask=mask)
+    ls = np.ma.compressed(ls)
+    td = np.ma.compressed(td)   
 
-    ##limit the lengh scale to 4-5km
-    #minlen = 3
-    #maxlen = 5
+    #limit the lengh scale to 4-5km
+    minlen = 3
+    maxlen = 5
 
-    #mask = (ls<minlen) | (ls>maxlen)
-    ##ls_class = np.ma.array(ls,mask=mask)
-    #td_class = np.ma.array(td,mask=mask)
-    ##ls_class = np.ma.compressed(ls_class)
-    #td_class = np.ma.compressed(td_class)  
+    mask = (ls<minlen) | (ls>maxlen)
+    ls_class = np.ma.array(ls,mask=mask)
+    td_class = np.ma.array(td,mask=mask)
+    ls_class = np.ma.compressed(ls_class)
+    td_class = np.ma.compressed(td_class)  
     
-    ##print td_class
-    ##exit()
+    #print td_class
+    #exit()
     
-    ##calculate and store averages
-    #meanls=i
-    #meantd=np.mean(td_class)
-    ##meanls_list.append(meanls)
-    #meantd_list_b.append(meantd)
+    #calculate and store averages
+    meanls=i
+    meantd=np.mean(td_class)
+    #meanls_list.append(meanls)
+    meantd_list_b.append(meantd)
     
-    ##print meanls, meantd
+    #print meanls, meantd
     
-    ##plot all the data
-    ##ax.scatter(ls_class, td_class, lw=0, alpha=.2)  # Data
-    #ax.plot(meanls,meantd,'o',markersize=7,markeredgecolor='k')
+    #plot all the data
+    #ax.scatter(ls_class, td_class, lw=0, alpha=.2)  # Data
+    ax.plot(meanls,meantd,'o',markersize=7,markeredgecolor='k')
 
 #SAR data
 reg = 'leg1'
-lscale = 50
-inpath = '../output/def_50/'
+lscale = 25
+inpath = '../output/def_'+str(lscale)+'/'
 outpath = '../plots/'
 
-minlen=2
-maxlen=4
+minlen=0
+maxlen=10
 
-tc_min = [.4,1,4,6,7,15,20]
-tc_max = [1,2,6,7,9,20,25]
+tc_min = [.4,1,4,6,7,15,20,25,50]
+tc_max = [1,2,6,7,9,20,25,50,80]
+
+
+#tc_min = [1,4,6,15,20,25,50]
+#tc_max = [2,6,7,20,25,50,80]
+
+#tc_min = [20,25,50]
+#tc_max = [25,50,80]
+
 
 meanls_list_sar=[]
 meantd_list_sar=[]
@@ -114,7 +124,7 @@ meantd_list_sar=[]
 
 f = inpath+'td_'+reg+'_L'+str(lscale)+'_15km.csv'
 #f = inpath+'td_'+reg+'_L'+str(lscale)+'_25km.csv'
-print(f)
+#print(f)
 
 #extract time lengh scale from the name of the file
 sls = getColumn(f,1, delimiter=',')
@@ -126,11 +136,23 @@ ls = np.array(ls,dtype=np.float)
 td = np.array(td,dtype=np.float)
 ang = np.array(ang,dtype=np.float)
 
+
+
 #mask out all the acute triangles
 mask=ang>15
 ls = ls[mask]/60/60  #convert from s to h
 td = td[mask]
 sls = sls[mask]/1000  #convert from m to km
+
+#np.set_printoptions(threshold=np.nan)
+#print(ls)
+#exit()
+
+#mask outliers
+mask = td<1
+ls = ls[mask]
+td = td[mask]
+sls = sls[mask]
 
 #mask all very small or big triangles
 #if not masked the renge of the ls is big and has several clouds (expected ls, twice the ls and all kinds of smaller ls)
@@ -162,19 +184,27 @@ for i in range(0,len(tc_min)):
     
     #plot means
     ax.plot(meanls,meantd,'*',markersize=10,markeredgecolor='k', color='r')
+    
+    print(meanls)
+    print(meantd)
 
 
 #dummy x data for plotting
+tc_min = [.4,1,4,6,7,15,20,25,50]
+tc_max = [1,2,6,7,9,20,25,50,80]
+
 x = (np.array(tc_min)+np.array(tc_max))/2
 
 
-#ship radar
-#fit the line
-a,k,cix,ciy_upp,ciy_low = logfit(meanls_list,meantd_list_sr)
 
-ax.loglog(x,a*x**k,linewidth=2,label=r'$D=%.2f L^{%.2f}$' %(a,k),c='m')
-ax.plot(cix,ciy_low,'--', c= 'r',linewidth=1,label=r'$99\%\,confidence\,band$')
-ax.plot(cix,ciy_upp,'--', c= 'r',linewidth=1)
+
+##ship radar
+##fit the line
+#a,k,cix,ciy_upp,ciy_low = logfit(meanls_list,meantd_list_sr)
+
+#ax.loglog(x,a*x**k,linewidth=2,label=r'$D=%.2f L^{%.2f}$' %(a,k),c='m')
+#ax.plot(cix,ciy_low,'--', c= 'r',linewidth=1,label=r'$99\%\,confidence\,band$')
+#ax.plot(cix,ciy_upp,'--', c= 'r',linewidth=1)
 
 ##buoys
 ##fit the line to bins
@@ -184,13 +214,13 @@ ax.plot(cix,ciy_upp,'--', c= 'r',linewidth=1)
 #ax.plot(cix,ciy_low,'--', c= 'r',linewidth=1,label=r'$99\%\,confidence\,band$')
 #ax.plot(cix,ciy_upp,'--', c= 'r',linewidth=1)
 
-##sar 
-##fit the line
-#a,k,cix,ciy_upp,ciy_low = logfit(meanls_list_sar,meantd_list_sar)
+#sar 
+#fit the line
+a,k,cix,ciy_upp,ciy_low = logfit(meanls_list_sar,meantd_list_sar)
 
-#ax.loglog(x,a*x**k,linewidth=2,label=r'$D=%.2f L^{%.2f}$' %(a,k),c='m')
-#ax.plot(cix,ciy_low,'--', c= 'r',linewidth=1,label=r'$99\%\,confidence\,band$')
-#ax.plot(cix,ciy_upp,'--', c= 'r',linewidth=1)
+ax.loglog(x,a*x**k,linewidth=2,label=r'$D=%.2f L^{%.2f}$' %(a,k),c='royalblue')
+ax.plot(cix,ciy_low,'--', c= 'r',linewidth=1,label=r'$99\%\,confidence\,band$')
+ax.plot(cix,ciy_upp,'--', c= 'r',linewidth=1)
 
 
 
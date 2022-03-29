fd99ddb9c82c2b99fd97ee7f7d2245b73f8e4ff8
loniitkina <geopolonica@gmail.com>
2022-03-29 16:02:34 +0200

    s-1 downloading

---
+++ sid_creodias_func.py
@@ -0,0 +1,180 @@
+import wget
+import requests
+from keycloak import KeycloakOpenID
+import keycloak
+import urllib
+import os
+import glob
+from osgeo import ogr#, osr
+import numpy as np
+import pathlib
+
+def download_from_polygon(polygon, start_date, end_date, out_folder, start_hour = '00', end_hour = '23', coverage=.8):
+    '''
+    This function downloads Sentinel-1 GRD products from Creodias
+    
+    Inputs:
+    -------
+    polygon: array_like
+        Lats and longs of the area of interest
+        format [lat1,lat2,lon1,lon2]
+    start_date: str_like
+        format 'YYYY-MM-DD'
+        The start date the observations of interest were made
+    end_date: string
+        format 'YYYY-MM-DD'
+        The end date the observations of interest were made
+    out_folder: string
+        Path of the folder where to download the S1 products
+    start_hour: string (optional)
+        format 'HH'
+        The start hour in the start_date the observations of interest were made
+    end_hour: string (optional)
+        format 'HH'
+        The end hour in the end_date the observations of interest were made
+        
+        
+    Outputs:
+    -------
+    r: bool
+        0 if the donwload did not succeed due to errors of connection / no good overlap
+        1 if the product is downloaded
+    '''
+    
+    if start_hour != '00':
+        end_hour = str(int(start_hour)+1)
+        if len(end_hour) == 1:
+            end_hour = f'0{end_hour}'
+            
+    path = os.path.dirname(__file__)
+    token = create_token()
+
+    # Remove temporary files
+    for temp_files in glob.glob(f'{path}/*.tmp', recursive = True):
+        os.remove(temp_files)
+
+    # Extract the downlod url
+    URL = f'https://finder.creodias.eu/resto/api/collections/Sentinel1/search.json?maxRecords=10&startDate={start_date}T{start_hour}%3A00%3A00Z&completionDate={end_date}T{end_hour}%3A59%3A59Z&productType=GRD&geometry=POLYGON(({polygon[2]}+{polygon[1]}%2C{polygon[3]}+{polygon[1]}%2C{polygon[3]}+{polygon[0]}%2C{polygon[2]}+{polygon[0]}%2C{polygon[2]}+{polygon[1]}))&sortParam=startDate&sortOrder=descending&status=all&dataset=ESA-DATASET'
+
+    r = find_products(URL)
+    if "ErrorCode" in r.json():
+        return 0
+    
+    if r.json()["properties"]["totalResults"] == 0:
+        print("No product fits with your quiery")
+        return 0
+
+    # Calculate intersection of the found products and the polygon of interest    
+    intersection=[]
+    for i in range(r.json()["properties"]["totalResults"]):
+        #print(i)
+        try:
+            inter = get_intersection(
+                r.json()["features"][i]["geometry"]["coordinates"][0], polygon)
+        except:
+            #some scenes dont have coodinates...
+            inter=0
+        intersection.append(inter)
+        
+    print('Intercestion fractions of individual scenes with your polygon: ',intersection)
+
+    id = np.argmax(intersection)
+    
+    # If all products have less than 80% (default) of overlap abort 
+    if intersection[id]<=coverage:
+        return 0
+    
+    # Check if the output folder exist 
+    if folder_exist(out_folder):
+        # If the product exist abort
+        if product_exist(out_folder, pathlib.Path(r.json()["features"][id]["properties"]["productIdentifier"]).stem):
+            return 1
+        
+    # Extract the url for download
+    url_download = f'{r.json()["features"][id]["properties"]["services"]["download"]["url"]}?token={token["access_token"]}'
+    
+    # Download file
+    pathlib.Path(out_folder).mkdir(parents=True, exist_ok=True)
+    r = download_products(url_download, out_folder)
+    return 0 if r is None else 1
+
+  
+def OpenID():
+    try:
+        r = KeycloakOpenID(server_url="https://auth.creodias.eu/auth/",
+                    client_id="CLOUDFERRO_PUBLIC",
+                    realm_name="DIAS")
+    except keycloak.exceptions.KeycloakError:
+        # Maybe set up for a retry, or continue in a retry loop
+        print('Timeout')
+        OpenID()
+    except keycloak.exceptions.KeycloakGetError:
+        # Maybe set up for a retry, or continue in a retry loop
+        print('Timeout')
+        OpenID()
+    return r
+
+def find_products(url: str):
+    try:
+        r = requests.get(url)
+    except requests.exceptions.Timeout:
+        # Maybe set up for a retry, or continue in a retry loop
+        print('Timeout')
+        r = find_products(url)
+    except requests.exceptions.TooManyRedirects:
+        return None
+    except requests.exceptions.RequestException as e:
+        # catastrophic error. bail.
+        raise SystemExit(e) from e
+    return r
+
+def download_products(url, out):
+    try:
+        r = wget.download(url, out)
+    except urllib.error.ContentTooShortError:
+        # Maybe set up for a retry, or continue in a retry loop
+        print('Content too short')
+        r = download_products(url, out)
+    except urllib.error.HTTPError:
+        r = None
+    return r
+
+def create_token():
+
+    keycloak_openid = OpenID()
+    print("Id opened")
+    return keycloak_openid.token("polona.itkin@uit.no", "GGP2D3j2uDZczxK!")
+
+
+def get_intersection(get_poly_im, polygon):
+    if len(get_poly_im) <5:
+        get_poly_im = get_poly_im[0]
+    if len(get_poly_im) >5:
+        poly_im = get_lat_long(np.asarray(get_poly_im))
+        wkt_im = f"POLYGON (({str(poly_im[2])} {str(poly_im[1])}, {str(poly_im[3])} {str(poly_im[1])}, {str(poly_im[3])} {str(poly_im[0])}, {str(poly_im[2])} {str(poly_im[0])}, {str(poly_im[2])} {str(poly_im[1])}))"
+    else:
+        wkt_im = f"POLYGON (({str(get_poly_im[0][0])} {str(get_poly_im[0][1])}, {str(get_poly_im[1][0])} {str(get_poly_im[1][1])}, {str(get_poly_im[2][0])} {str(get_poly_im[2][1])}, {str(get_poly_im[3][0])} {str(get_poly_im[3][1])}, {str(get_poly_im[4][0])} {str(get_poly_im[4][1])}))"
+
+    wkt_oil = f"POLYGON (({str(polygon[2])} {str(polygon[1])}, {str(polygon[3])} {str(polygon[1])}, {str(polygon[3])} {str(polygon[0])}, {str(polygon[2])} {str(polygon[0])}, {str(polygon[2])} {str(polygon[1])}))"
+    
+    poly_oil = ogr.CreateGeometryFromWkt(wkt_oil)
+    poly_im = ogr.CreateGeometryFromWkt(wkt_im)
+
+    intersection = poly_oil.Intersection(poly_im)
+    if intersection is None: 
+        return 0
+    else: return intersection.GetArea()/poly_oil.GetArea()
+
+def folder_exist(folder_name):
+    return bool(os.path.exists(f'{folder_name}'))
+
+def product_exist(folder_name, product_name):
+    return bool(os.path.exists(f'{folder_name}{product_name}.SAFE') or os.path.exists(f'{folder_name}{product_name}.zip'))
+
+def get_lat_long(array):
+    if len(array.shape) == 2:
+        return np.min(array[:,1]), np.max(array[:,1]), np.min(array[:,0]), np.max(array[:,0])
+    else: return np.min(array[0,0,:,1]), np.max(array[0,0,:,1]), np.min(array[0,0,:,0]), np.max(array[0,0,:,0])   
+    
+
+    
---
+++ sid_creodias_main.py
@@ -0,0 +1,66 @@
+import numpy as np
+from datetime import datetime
+from sid_func import getColumn
+from pyproj import Proj, transform
+from sid_creodias_func import download_from_polygon
+
+#This script will make a polygon of 100x100km around Polarstern and download the S-1 scenes
+
+#read the PS positions
+ps_file='../transect/data/floenavi/data_master-solution_mosaic-leg1-20191016-20191213-floenavi-refstat-v1p0.csv'
+ps_file='../transect/data/floenavi/data_master-solution_mosaic-leg2-20191214-20200224-floenavi-refstat-v1p0.csv'
+#ps_file='../coord_trans/dshipextracts/transect_legs/position_leg3_nh-track.csv'
+
+dt = getColumn(ps_file,0)
+lon = getColumn(ps_file,1)
+lat = getColumn(ps_file,2)
+lon = np.array(lon,dtype=np.float)
+lat = np.array(lat,dtype=np.float)
+dt = [ datetime.strptime(x, "%Y-%m-%d %H:%M:%S") for x in dt ]
+
+#get 12UTC positions
+full_hour= np.where(np.array([ x.minute for x in dt ])==0,1,0)
+noon=np.where(np.array([ x.hour for x in dt ])==12,1,0)*full_hour
+
+lat_noon=np.ma.array(lat,mask=noon==0).compressed()
+lon_noon=np.ma.array(lon,mask=noon==0).compressed()
+dt_noon=np.ma.array(dt,mask=noon==0).compressed()
+
+outProj = Proj(init='epsg:4326')
+FloeNaviProj = Proj('+proj=stere +lat_0=%f +lon_0=%f +x_0=0 +y_0=0 +ellps=WGS84'%(0,0))
+radius=50000    #50km
+
+#cycle through every day
+for i in range(0,len(dt_noon)):
+    #convert to geogaphical coordinates
+    x,y = transform(outProj,FloeNaviProj,lon_noon[i],lat_noon[i])
+    
+    #get corners
+    ul_corner_x=x-radius
+    ul_corner_y=y+radius
+    lr_corner_x=x+radius
+    lr_corner_y=y-radius
+
+    #convert back to lat,lon
+    ul_lon,ul_lat = transform(FloeNaviProj,outProj,ul_corner_x,ul_corner_y)
+    lr_lon,lr_lat = transform(FloeNaviProj,outProj,lr_corner_x,lr_corner_y)
+
+    #write polygon with UL and LR coordinates
+    #format [lat1,lat2,lon1,lon2]
+    polarstern_poly = [ul_lat,lr_lat,ul_lon,lr_lon]
+    print(polarstern_poly)
+
+    #get the date
+    start_date=datetime.strftime(dt_noon[i], '%Y-%m-%d')
+    end_date=start_date
+    
+    print(start_date)
+    
+    continue
+    
+    out_folder='/home/polona/Downloads/'
+
+    output = download_from_polygon(polarstern_poly, start_date, end_date, out_folder, start_hour = '00', end_hour = '23', coverage=.5)
+    print(output)
+    
+#alternatives: get mosaics from sentinelhub

6802485fc175c33e2b134cecb3abbfebd7725d94
loniitkina <geopolonica@gmail.com>
2023-09-15 11:20:33 +0200

    two versions of get token

--- sid_creodias_func.py
+++ sid_creodias_func.py
@@ -198,8 +198,7 @@ def _get_token(aut2, username="polona.itkin@uit.no", password="GGP2D3j2uDZczxK!"
         'username': username,
         'password': password,
         'grant_type': 'refresh_token',
-        'refresh_token': refresh_token,
-        'totp': None
+        'refresh_token': refresh_token
     }
     if len(refresh_token) > 0:  # don't bother API if we know it isn't working because no refresh token
         response = requests.post(URL_TOKEN, data=token_data).json()
@@ -209,17 +208,16 @@ def _get_token(aut2, username="polona.itkin@uit.no", password="GGP2D3j2uDZczxK!"
         token = response['access_token']
     except KeyError:  # we didn't find a refresh token or it's expired
         token_data['grant_type'] = 'password'  # renew login with password
-        if aut2:
-            token_data['totp'] = aut2
-        else:
-            token_data['totp'] = input(f'****** Provide the 2-factor authentication token for user "{username}":   ')  # ask user for token from App on phone
-        # token_data['totp'] = aut2response = requests.post(URL_TOKEN, data=token_data).json()  # get token
+        token_data['totp'] = input(f'****** Provide the 2-factor authentication token for user "{username}":   ')  # ask user for token from App on phone
+        #token_data['totp'] = aut2
+        del token_data['refresh_token']  #  the disfunctional refresh token should not be provided
+        response = requests.post(URL_TOKEN, data=token_data).json()  # get token
         try:
             token = response['access_token']  # it should be there
         except KeyError:
             raise RuntimeError(f'Unable to get token. Response from CREODIAS was {response}')  # could be wrong user credentials, or service is down etc.
-    #finally:
-        #_update_refresh_token(response['refresh_token'], username)  # response contains a refresh token, we keep it to maximize the time we don't ask the user for a 2-factor authentication token
+    finally:
+        _update_refresh_token(response['refresh_token'], username)  # response contains a refresh token, we keep it to maximize the time we don't ask the user for a 2-factor authentication token
     #print(token)
     #exit()
     return token
@@ -236,6 +234,11 @@ def _get_refresh_token(username):
         return ''  
 
 if __name__ == '__main__':
+    dir_out = os.path.dirname(__file__)
     username, password = os.environ["CREO_USER"], os.environ["CREO_PASSWORD"]
-    kwargs = dict(username=username, password=password, oauth_token=sys.argv[1])
+    file_zip = os.path.join(dir_out, "test_to_be_deleted.zip")
+    kwargs = dict(username=username, password=password)
     token = _get_token(**kwargs)  # test get token
+    token = _get_token(**kwargs)  # retry test to see if it works with refresh token
+    download(uid="a8eebdc2-31bb-57fa-8f96-3aeae4f2cdaf", username=username, password=password, outfile=file_zip)  # test if download also works
+    os.remove(file_zip)  # delete downloaded data
